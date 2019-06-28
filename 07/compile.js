e3fc562a4cfe482156edd822cc2ffcdb49cb13c9
liyong <1528487943@qq.com>
2019-06-28 21:16:18 +0800

    周五项目更新

--- 07/compile.js
+++ 07/compile.js
@@ -51,22 +51,53 @@ class Compile {
     }
 
     // 编译元素节点
-    compileElement(node){
+    compileElement(node) {
         console.log('编译元素节点');
         //<div k-test='test' @click='onClick'></div>
-        const attrs=node.attributes;
+        const attrs = node.attributes;
         // Array.from()将类数组元素转换成数组进行遍历：最基本的要求就是具有length属性的对象。
-        Array.from(attrs).forEach(arr=>{
+        Array.from(attrs).forEach(attr => {
+            //规定指令k-text='test' @click='onClick'
+            const attrName = attr.name;// 属性名k-text
+            const exp = attr.value;// 属性值test
+            if (this.isDirective(attrName)) {// 指令text
+                const dir = attrName.substr(2);//text
+                this[dir] && this[dir](node,this.$vm,exp);
+            } else if(this.isEventDirective(attrName)){// 事件
+                const dir = attrName.substr(1);//click
+                this.eventHandler(node,this.$vm,exp,dir);
+            }
 
         })
 
     }
-    // 编译文本节点
-    compileText(node){
 
-        console.log('编译文本节点');
+    // 指令判断
+    isDirective(attr){
+        return attr.indexOf('k-')==0;
+    }
+
+    // 事件判断
+    isEventDirective(attr){
+        return attr.indexOf('@')==0;
+    }
+
+    // 文本更新
+    text(node,vm,exp){
 
     }
+    //事件处理
+    eventHandler(){
+
+    }
+
+    // 编译文本节点
+    compileText(node,exp) {
+        // console.log('编译文本节点');
+        this.text(node,this.$vm,exp);
+
+    }
+
     isElementNode(node) {
         return node.nodeType == 1;//元素节点
     }

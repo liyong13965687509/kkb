7b518ea9903158f4d207e8e09b70ecea9d4bdb06
liyong <13965687509@163.com>
2019-07-05 18:24:41 +0800

    更新

--- 08/reactLearn/react01/src/components/Hoc.js
+++ 08/reactLearn/react01/src/components/Hoc.js
@@ -11,9 +11,19 @@ function Kaikeba(props) {
 
 // 高阶组件---对参数组件获取数据的能力进行扩展
 const withName = Comp => {
+    // 甚至可以重写组件的生命周期
+    class NewComponent extends Component {
+        componentDidMount() {
+            console.log('do something');
+        }
+        render(){
+            return <Comp {...this.props} name="高阶组件使用介绍"/>
+        }
+    }
+
     // 假设通过某种特殊手段获取了本节课名字
     // return的不是JSX，而是一个组件(这里是一个函数形式的组件)
-    return props => <Comp {...props} name="高阶组件使用介绍"/>;
+    return NewComponent;
 }
 
 // class Hoc extends Component {

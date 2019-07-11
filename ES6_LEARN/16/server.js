30c26a97d9818388bd4253f5d2101436a518cf86
liyong <13965687509@163.com>
2019-07-11 13:29:48 +0800

    更新

---
+++ ES6_LEARN/16/server.js
@@ -0,0 +1,16 @@
+const koa = require('koa');
+const mysql = require('koa-mysql');
+
+let db = mysql.createPool({host: 'localhost', user: 'root', password: '123455', database: '20170702'})
+
+let server = new koa();
+
+
+server.use(function* () {
+    let data = yield db.query('SELECT * FROM user_table');
+
+    this.body=data;
+});
+
+
+server.listen(8080);
